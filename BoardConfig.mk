d19691525726bdaa479a43cb804658b7a8e05d03
YZ-Bruh <131263805+YZ-Bruh@users.noreply.github.com>
2023-08-31 22:42:57 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,6 +89,7 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+TW_DEVICE_VERSION := YZ-Bruh
 TW_DEFAULT_DEVICE_NAME := gtanotexlwifikx
 TW_DEFAULT_LANGUAGE := en
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
